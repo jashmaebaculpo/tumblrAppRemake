5e8330ffd46f5b1c3b8cb6f74ca51e4c35518b4c
Jashmae <jbaculpo@csumb.edu>
2019-01-30 15:46:37 -0800

    adding the files we need to work on

---
+++ tumblrLab1/tumblrLab1/PhotoViewController.swift
@@ -0,0 +1,65 @@
+//
+//  PhotoViewController.swift
+//  tumblrLab1
+//
+//  Created by Jashmae Baculpo on 1/30/19.
+//  Copyright © 2019 Jashmae Baculpo. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell", for: indexPath) as! TableViewCell
+        
+        return cell
+    }
+    
+    // 1.       2.             3.
+    var posts: [[String: Any]] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        PhotoFeedView.delegate = self
+        PhotoFeedView.dataSource = self
+        
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                print(dataDictionary)
+                
+                // Get the dictionary from the response key
+                let responseDictionary = dataDictionary["response"] as! [String: Any]
+                // Store the returned array of dictionaries in our posts property
+                self.posts = responseDictionary["posts"] as! [[String: Any]]
+
+            }
+        }
+        task.resume()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBOutlet weak var PhotoFeedView: UITableView!
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
